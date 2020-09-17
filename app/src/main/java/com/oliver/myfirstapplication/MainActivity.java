e0ddb1046cfd159ee92e6c8358bd85cb4c5a01a8
localhostPIB <obosslet@htwsaar.de>
2020-09-17 19:48:54 +0200

    Playground Table

--- app/src/main/java/com/oliver/myfirstapplication/MainActivity.java
+++ app/src/main/java/com/oliver/myfirstapplication/MainActivity.java
@@ -1,32 +1,63 @@
 package com.oliver.myfirstapplication;
 
-import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.*;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+
+public class MainActivity extends Activity {
+   // private final static String[] tvArray = new String[]{"RTL","ARD", "ZDF"};
+    private final static Map tvMap = new HashMap<Integer, String>();
+    private TableRow tableRow;
+    private TableLayout tableLayout;
+
+
+    static {
+        tvMap.put(22 ,"RTL");
+        tvMap.put(78, "ZDF");
+        tvMap.put(64, "SR");
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        final Button button1    = findViewById(R.id.button2);
-       final TextView text      = findViewById(R.id.text1) ;
+        tableLayout = findViewById(R.id.mainTable);
 
 
-        button1.setOnClickListener(new View.OnClickListener() {
+        fillIn();
 
-            @Override
-            public void onClick(View view) {
-                text.setText("Lets Go !!!");
+    }
 
-            }
-        });
+    private void fillIn(){
+        Context ctx = getApplicationContext();
+        Iterator i = tvMap.entrySet().iterator();
+        while(i.hasNext()){
+            tableRow = new TableRow(ctx);
+            tableRow.addView(createTextView(i.next().toString()));
+            tableLayout.addView(tableRow);
+        }
+    }
+
+   private TextView createTextView(String text){
+       TextView textView = new TextView(getApplicationContext());
+       String rowplace   = text.substring(0,2);
+       String rowChannel = text.substring(3);
+       String row        ="\t\t"+ rowplace
+               + "\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t" + rowChannel;
+
+       textView.setText(row);
+
+       return  textView;
+   }
+
     }
 
 
-}

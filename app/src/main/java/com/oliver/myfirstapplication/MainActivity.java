b0aff27e863e3a30dfbd025a099665c20aff8dd1
localhostPIB <obosslet@htwsaar.de>
2020-09-18 12:11:48 +0200

    Playground Table #2

--- app/src/main/java/com/oliver/myfirstapplication/MainActivity.java
+++ app/src/main/java/com/oliver/myfirstapplication/MainActivity.java
@@ -19,12 +19,99 @@ public class MainActivity extends Activity {
 
 
     static {
+        tvMap.put(1, "HSE 24");
+        tvMap.put(2, "Welt");
         tvMap.put(3, "Sat.1");
         tvMap.put(4, "EuroNews");
+        tvMap.put(5, "Sport 1");
+        tvMap.put(6, "Kabel 1");
         tvMap.put(7, "Pro 7");
+        tvMap.put(8, "servus TV");
+        tvMap.put(9, "Juwelo TV");
+        tvMap.put(10, "DMAX");
+        tvMap.put(11, "QVC");
+        tvMap.put(12, "Sonnenklar TV");
+        tvMap.put(13, "Al Jazeera Int");
+        tvMap.put(14, "CNN");
+        tvMap.put(15, "Astro TV");
+        tvMap.put(16, "Comedy Central");
+        tvMap.put(17, "EuroSport");
+        tvMap.put(18, "N-TV");
+        tvMap.put(19, "VOX");
+        tvMap.put(20, "RTL 2");
+        tvMap.put(21, "Super RTL");
         tvMap.put(22 ,"RTL");
-        tvMap.put(78, "ZDF");
+        tvMap.put(23, "France 3");
+        tvMap.put(24, "Melodie TV");
+        tvMap.put(25, "Channel 21");
+        tvMap.put(26, "????");
+        tvMap.put(27, "VOXup");
+        tvMap.put(28, "?????");
+        tvMap.put(29, "Toggo plus");
+        tvMap.put(30, "Sat.1 Gold");
+        tvMap.put(31, "DELUXE Music");
+        tvMap.put(32, "TLC");
+        tvMap.put(33, "???");
+        tvMap.put(34, "Anixe");
+        tvMap.put(35, "Nitro");
+        tvMap.put(36, "TV 5 Monde");
+        tvMap.put(37, "Nick");
+        tvMap.put(38, "???");
+        tvMap.put(39, "Tele 5");
+        tvMap.put(40, "BBC World News");
+        tvMap.put(41, "???");
+        tvMap.put(42, "???");
+        tvMap.put(43, "Sparhandy TV");
+        tvMap.put(44, "France 5");
+        tvMap.put(45, "Welt der Wunder TV");
+        tvMap.put(46, "N24 Doku");
+        tvMap.put(47, "QVC 2");
+        tvMap.put(48, "sixx");
+        tvMap.put(49, "Kabel 1 Doku");
+        tvMap.put(51, "Disney Channel");
+        tvMap.put(52, "123 tv");
+        tvMap.put(53, "Sonnenklar TV");
+        tvMap.put(54, "SR HD");
+        tvMap.put(55, "tagesschau 24");
+        tvMap.put(56, "ARD Alpha");
+        tvMap.put(57, "QVC HD");
+        tvMap.put(58, "HSE 24");
+        tvMap.put(59, "Phoenix");
+        tvMap.put(60, "Arte");
+        tvMap.put(61, "ONE");
+        tvMap.put(62, "Tagesschau 24");
+        tvMap.put(63, "ARD Alpha");
         tvMap.put(64, "SR");
+        tvMap.put(65, "WDR Köln");
+        tvMap.put(66, "MDR");
+        tvMap.put(67, "NDR");
+        tvMap.put(68, "rbb");
+        tvMap.put(69, "SWR");
+        tvMap.put(70, "WDR");
+        tvMap.put(71, "HR");
+        tvMap.put(72, "BR");
+        tvMap.put(73, "ARD");
+        tvMap.put(74, "ZDF NEO");
+        tvMap.put(75, "ZDF info");
+        tvMap.put(76, "Kika");
+        tvMap.put(77, "3 Sat");
+        tvMap.put(78, "ZDF");
+        tvMap.put(79, "ZDF info");
+        tvMap.put(80, "KIKA HD");
+        tvMap.put(81, "ZDF Info HD");
+        tvMap.put(82, "ARTE HD");
+        tvMap.put(83, "ZDF HD");
+        tvMap.put(84, "ARD HD");
+        tvMap.put(85, "ONE HD");
+        tvMap.put(86, "HR HD");
+        tvMap.put(87, "MDR HD");
+        tvMap.put(88, "RBB HD");
+        tvMap.put(89, "PHOENIX HD");
+        tvMap.put(90, "NDR HD");
+        tvMap.put(91, "BR HD");
+        tvMap.put(92, "SWR HD");
+        tvMap.put(93, "SKY Sport News HD");
+        tvMap.put(94, "SKY Sport News");
     }
 
     @Override
@@ -34,9 +121,7 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
         tableLayout = findViewById(R.id.mainTable);
 
-
         fillIn();
-
     }
 
     private void fillIn(){
